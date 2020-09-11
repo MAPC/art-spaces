4bd0c3329a45b9803398122518ed6e28fd4988b8
Matthew Zagaja <mzagaja@mapc.org>
2020-09-11 11:37:50 -0400

    Spike Working Map + Apollo Request
    This commit spikes a working/loading Map from Mapbox along with the initial request from the GraphQL API (in development) so that we can see the data make its way to the client.
    
    We refactor the Map class component into a more modern React hook because Apollo now relies on React Hooks and the class based components aren’t compatible with it (at least not in the way they documented).

--- app/javascript/pages/App.jsx
+++ app/javascript/pages/App.jsx
@@ -1,9 +1,19 @@
 import React from 'react';
 import Map from './components/map';
 
+import { ApolloProvider } from '@apollo/client';
+import { ApolloClient, InMemoryCache } from '@apollo/client';
+
+const client = new ApolloClient({
+  uri: 'http://localhost:3000/graphql',
+  cache: new InMemoryCache()
+});
+
 const App = () => (
   <>
-    <Map />
+    <ApolloProvider client={client}>
+      <Map />
+    </ApolloProvider>
   </>
 );
 
--- app/javascript/pages/components/map.jsx
+++ app/javascript/pages/components/map.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import ReactMapGL from 'react-map-gl';
 import '../../styles/map.scss';
 import { useQuery, gql } from '@apollo/client';
@@ -6,36 +6,38 @@ import { useQuery, gql } from '@apollo/client';
 const SITES = gql`
   query GetSites {
     sites {
-      id
-      siteName
-      location
+      type
+      features {
+        type
+        geometry
+        properties
+      }
     }
   }
 `;
 
-export default class Map extends Component {
-  state = {
-    viewport: {
+const Map = () => {
+  const [viewport, setViewport] = useState(
+    {
       latitude: 42.3601,
       longitude: -71.0589,
       zoom: 10
-    }
-  };
+    });
 
-  componentDidMount() {
-    const { loading, error, data } = useQuery(SITES);
-  }
+  const { loading, error, data } = useQuery(SITES);
 
+  if (loading) return 'Loading...';
+  if (error) return `Error! ${error.message}`;
 
-  render() {
-    return (
-      <ReactMapGL
-        {...this.state.viewport}
-        width="100vw"
-        height="100vh"
-        onViewportChange={(viewport) => this.setState({viewport})}
-        mapboxApiAccessToken={process.env.MAPBOX_API_TOKEN}
-      />
-    );
-  }
+  return (
+    <ReactMapGL
+      {...viewport}
+      width="100vw"
+      height="100vh"
+      onViewportChange={(viewport) => setViewport(viewport)}
+      mapboxApiAccessToken={process.env.MAPBOX_API_TOKEN}
+    />
+  )
 }
+
+export default Map;
