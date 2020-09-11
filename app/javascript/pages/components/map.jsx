35b7ac939f906a2796cf0bed6bcefd9950f122ee
Matthew Zagaja <mzagaja@mapc.org>
2020-09-11 17:09:46 -0400

    Implement 3D Building Feature
    * Add pitch, bearing, etc. to viewport in order to align view for building extrusions
    * Add a buildingLayer style to be appended to the map in order to show 3D buildings.
    * Add onClick event to begin inspecting results of clicks on the map.
    * Update ids of Source and layer in order to reference them more easily.

--- app/javascript/pages/components/map.jsx
+++ app/javascript/pages/components/map.jsx
@@ -23,18 +23,55 @@ const Map = () => {
     {
       latitude: 42.3601,
       longitude: -71.0589,
-      zoom: 10
+      zoom: 10,
+      pitch: 45,
+      bearing: -17.6,
+      antialias: true
     });
 
 
   const [dataLayer, setDataLayer] = useState({
-      id: 'my-layer',
+      id: 'art-site-points',
       type: 'circle',
       source: 'points',
       paint: {
           'circle-color': '#f00',
           'circle-radius': 4
       }
+  })
+
+  const [buildingLayer, setBuildingLayer] = useState({
+    'id': '3d-buildings',
+    'source': 'composite',
+    'source-layer': 'building',
+    'filter': ['==', 'extrude', 'true'],
+    'type': 'fill-extrusion',
+    'minzoom': 15,
+    'paint': {
+      'fill-extrusion-color': '#aaa',
+
+      // use an 'interpolate' expression to add a smooth transition effect to the
+      // buildings as the user zooms in
+      'fill-extrusion-height': [
+      'interpolate',
+      ['linear'],
+      ['zoom'],
+      15,
+      0,
+      15.05,
+      ['get', 'height']
+      ],
+      'fill-extrusion-base': [
+      'interpolate',
+      ['linear'],
+      ['zoom'],
+      15,
+      0,
+      15.05,
+      ['get', 'min_height']
+      ],
+      'fill-extrusion-opacity': 0.6
+    }
   })
 
   if (loading) return 'Loading...';
@@ -48,10 +85,12 @@ const Map = () => {
       onViewportChange={(viewport) => setViewport(viewport)}
       mapboxApiAccessToken={process.env.MAPBOX_API_TOKEN}
       mapStyle="mapbox://styles/mapbox/light-v10"
+      onClick={(event) => {console.log(event.features)}}
     >
-      <Source type="geojson" data={data.sites}>
+      <Source type="geojson" data={data.sites} id="art-sites">
         <Layer {...dataLayer} />
       </Source>
+      <Layer {...buildingLayer} />
     </ReactMapGL>
   )
 }
