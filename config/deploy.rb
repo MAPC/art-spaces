157bc9e816824fc4a72e389c5256a654629b9e24
Matthew Zagaja <mzagaja@alum.wpi.edu>
2020-12-16 23:22:41 +0000

    Add master.key to deployment * To fix a missing secret key base error we need to add master.key to the deployment list of linked files.

--- config/deploy.rb
+++ config/deploy.rb
@@ -21,7 +21,7 @@ set :repo_url, 'git@github.com:MAPC/art-spaces.git'
 # set :pty, true
 
 # Default value for :linked_files is []
-append :linked_files, 'config/database.yml'
+append :linked_files, 'config/database.yml', 'config/master.key'
 
 # Default value for linked_dirs is []
 append :linked_dirs,
